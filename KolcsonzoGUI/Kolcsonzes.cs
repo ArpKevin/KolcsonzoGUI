718246aacea079e8813d863021f9ecd921a648fd
ArpKevin <arpaskevin11@gmail.com>
2025-01-24 14:02:09 +0100

    Add project files.

---
+++ KolcsonzoGUI/Kolcsonzes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolcsonzoGUI
+{
+    internal class Kolcsonzes
+    {
+        public string Nev { get; set; }
+        public byte HajoAzonosito { get; set; }
+        public string HajoTipus { get; set; }
+        public byte SzemelyekSzama { get; set; }
+        public byte ElvitelOraja { get; set; }
+        public byte ElvitelPerce { get; set; }
+        public byte VisszahozatalOraja { get; set; }
+        public byte VisszahozatalPerce { get; set; }
+
+        public TimeSpan elvitelIdoben => TimeSpan.Parse($"{this.ElvitelOraja}:{this.ElvitelPerce}");
+        public TimeSpan visszahozatalIdoben => TimeSpan.Parse($"{this.VisszahozatalOraja}:{this.VisszahozatalPerce}");
+
+        //public bool vizenVanAHajo()
+        //{
+        //    return 
+        //}
+
+        public Kolcsonzes(string sor)
+        {
+            var x = sor.Split(";");
+            Nev = x[0];
+            HajoAzonosito = byte.Parse(x[1]);
+            HajoTipus = x[2];
+            SzemelyekSzama = byte.Parse(x[3]);
+            ElvitelOraja = byte.Parse(x[4]);
+            ElvitelPerce = byte.Parse(x[5]);
+            VisszahozatalOraja = byte.Parse(x[6]);
+            VisszahozatalPerce = byte.Parse(x[7]);
+        }
+    }
+}
---
+++ KolcsonzoGUI/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KolcsonzoGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Kolcsonzes> kolcsonzesek = new();
+            foreach (var item in File.ReadAllLines(@"..\..\..\src\kolcsonzes.txt").Skip(1))
+            {
+                kolcsonzesek.Add(new Kolcsonzes(item));
+            }
+
+            osszesRekord.ItemsSource = kolcsonzesek;
+
+            asd.Content = string.Format("{0:HH:mm:ss}", DateTime.Now);
+        }
+    }
+}
