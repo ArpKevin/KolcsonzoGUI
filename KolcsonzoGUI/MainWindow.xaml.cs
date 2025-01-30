e83a538f944d1b807aa8d6f3356681bb5f178c91
ArpKevin <arpaskevin11@gmail.com>
2025-01-30 13:14:10 +0100

    asd

--- KolcsonzoGUI/MainWindow.xaml.cs
+++ KolcsonzoGUI/MainWindow.xaml.cs
@@ -46,14 +46,35 @@ namespace KolcsonzoGUI
                 kolcsonzesSzamitottAdataiListbox.Items.Add(item.ToString());
             }
 
-            var felviteliLista = kolcsonzesek.Select(k => new { Azonosito = k.HajoAzonosito, Hajotipus = k.HajoTipus, SzemelyekSzama = k.SzemelyekSzama, ElvitelOraja = k.ElvitelOraja, ElvitelPerce = k.ElvitelPerce, VisszahozatalOraja = k.VisszahozatalOraja, VisszahozatalPerce = k.VisszahozatalPerce });
-
-
+            azonCombobox.ItemsSource = kolcsonzesek.Select(k => k.HajoAzonosito).Distinct();
+            tipusCombobox.ItemsSource = kolcsonzesek.Select(k => k.HajoTipus).Distinct();
+            szemelyekSzamaCombobox.ItemsSource = kolcsonzesek.Select(k => k.SzemelyekSzama).Distinct();
+            //asd.Content = valosHajo(1, "kajak", 3);
         }
 
         private void napiBevetel_Click(object sender, RoutedEventArgs e)
         {
             napiBevetelLabel.Content = kolcsonzesek.Sum(k => k.hanyMegkezdettFelOra()) * 1500;
+        }
+
+
+        public bool valosHajo(byte hajoAzon, string hajoTipus, byte szemelyekSzama)
+        {
+            return kolcsonzesek.Exists(k => k.HajoAzonosito == hajoAzon && k.HajoTipus == hajoTipus && k.SzemelyekSzama == szemelyekSzama);
+        }
+
+        private void felvitelGomb_Click(object sender, RoutedEventArgs e)
+        {
+            var nev = nevTextbox.Text;
+
+            var azon = azonCombobox.SelectedItem;
+            var tipus = tipusCombobox.SelectedItem;
+            var szemelyekSzama = szemelyekSzamaCombobox.SelectedItem;
+
+            var elvitelOraja = elvitelOrajaTextbox.Text;
+            var elvitelPerce = elvitelPerceTextbox.Text;
+            var visszahozatalOraja = visszahozatalOrajaTextbox.Text;
+            var visszahozatalPerce = visszahozatalPerceTextbox.Text;
         }
     }
 }
